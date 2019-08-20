26d9410a0beec7cbc10d3f590bf1fe51486f06a0
macotok <monochromer@r8.ucom.ne.jp>
2019-08-20 12:04:49 +0900

    新規投稿画面のファイル作成

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
       <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
         <Route exact path="/" component={EventsIndex} />
-        <Route exact path="events/new" component={EventsNew} />
       </Switch>
     </BrowserRouter>
   </Provider>,
