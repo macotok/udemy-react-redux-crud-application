019f6c2d806f78aa672b9b471b6912c4d5193469
macotok <monochromer@r8.ucom.ne.jp>
2019-08-24 15:09:01 +0900

    詳細画面にapiのdataとstateのdataを設定

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import {
   READ_EVENTS,
+  READ_EVENT,
   DELETE_EVENT,
 } from '../actions';
 
@@ -8,6 +9,9 @@ export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id');
+    case READ_EVENT:
+      const data = action.response.data;
+      return { ...events, [data.id]: data };
     case DELETE_EVENT:
       delete events[action.id];
       return { ...events };
